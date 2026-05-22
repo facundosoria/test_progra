df751cb5502fb263a51daddbc2fc9269abda0d07
Facundo Soria <facundo.cba2@gmail.com>
2026-05-22 00:52:14 -0300

    Tag

---
+++ api/src/main/java/com/codemon/CodemonApiApplication.java
@@ -0,0 +1,12 @@
+package com.codemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodemonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodemonApiApplication.class, args);
+    }
+}
